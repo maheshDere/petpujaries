1b20e9c98eaf6471ace70ca9a8af44c84bd2320e
Rahul Shewale <rahul.s.aux@go-jek.com>
2020-03-02 16:26:13 +0530

    add logger to log errors

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"petpujaris/config"
+	"petpujaris/logger"
 )
 
 func main() {
 	if err := config.SetupConfig(); err != nil {
 		panic(err)
 	}
+
 	config.LoadConfig()
+	logger.Setup()
+	logger.LogError(errors.New("err"), "main", "can not run script")
+	fmt.Println(config.GetAppConfig())
 
 }
