ef81f91acb3dc1f11ef638e309956f974ad31f8c
Rahul Shewale <rahul.s.aux@go-jek.com>
2020-03-02 15:33:44 +0530

    setup viper for config params

--- config/config.go
+++ config/config.go
@@ -2,5 +2,5 @@ package config
 
 type AppConfig struct {
 	ServerPort int
-	LogFile    string
+	LogFile    string `json: required`
 }
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"petpujaris/config"
+)
+
+func main() {
+	if err := config.SetupConfig(); err != nil {
+		panic(err)
+	}
+	config.LoadConfig()
+	fmt.Println(config.GetAppConfig())
+}
