16cc4712a949baa16292d8bcf427210ff4c3c37f
mahesh dere <mahesh.dere@joshsoftware.com>
2020-03-03 13:25:26 +0530

    add method to get db connection

--- repository/db_client.go
+++ repository/db_client.go
@@ -1,1 +1,39 @@
 package repository
+
+import (
+	"petpujaris/config"
+
+	"petpujaris/logger"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type PgClient struct {
+	db *sqlx.DB
+}
+
+func NewPgClient(dbcfg config.DatabaseConfig) (PgClient, error) {
+	db, err := GetDBConnection(dbcfg)
+	if err != nil {
+		logger.LogError(err, "postgres new client", "create new connection failure")
+		return PgClient{}, err
+	}
+
+	return PgClient{db}, nil
+}
+
+func GetDBConnection(dbcfg config.DatabaseConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", dbcfg.DataSourceURL())
+	if err != nil {
+		logger.LogError(err, "postgres client", "Opening connection failure")
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		logger.LogError(err, "postgres client", "DB Ping failure")
+		return nil, err
+	}
+
+	db.SetMaxIdleConns(dbcfg.MaxIdleConnections)
+	return db, nil
+}
