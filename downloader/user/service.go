f1bfa1b4dfc004c110b3d0ea18ba7c05089661ef
mahesh dere <mahesh.dere@joshsoftware.com>
2020-03-16 16:20:15 +0530

    add method to downloader.user service to create user primary detils

---
+++ downloader/user/service.go
@@ -0,0 +1,51 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"petpujaris/logger"
+	"petpujaris/repository"
+	"strconv"
+)
+
+type userFileService struct {
+	UserRegistry repository.UserRegistry
+}
+
+type UserFileService interface {
+	GetPrimaryUserDetails(ctx context.Context, adminID, totalEmployeeCount uint64) ([][]string, error)
+}
+
+const USER_ROLL_ID = "1"
+const USER_RESOURCEABLE_TYPE = "Company"
+
+func (ufs userFileService) GetPrimaryUserDetails(ctx context.Context, adminID, totalEmployeeCount uint64) ([][]string, error) {
+	primaryUserDetails := make([][]string, 0)
+	ResourceableID, err := ufs.UserRegistry.GetResourceableID(ctx, adminID)
+	if err != nil {
+		logger.LogError(err, "downloader.user", fmt.Sprintf("ResourceableID id not found for user with id %v", adminID))
+		return primaryUserDetails, err
+	}
+
+	primaryUserDetails = createPrimaryUserData(ctx, ResourceableID, totalEmployeeCount)
+	return primaryUserDetails, nil
+}
+
+func createPrimaryUserData(ctx context.Context, ResourceableID, totalEmployeeCount uint64) (primaryUserDetails [][]string) {
+	strResourceableID := strconv.FormatUint(ResourceableID, 10)
+	primaryUserDetails = make([][]string, 0)
+
+	headers := []string{"name", "email", "mobile_number", "is_active", "role_id", " resourceable_id", " resourceable_type"}
+	primaryUserDetails = append(primaryUserDetails, headers)
+
+	for empCount := uint64(0); empCount < totalEmployeeCount; empCount++ {
+		user := []string{"", "", "", "true", USER_ROLL_ID, strResourceableID, USER_RESOURCEABLE_TYPE}
+		primaryUserDetails = append(primaryUserDetails, user)
+	}
+
+	return
+}
+
+func NewUserFileService(ur repository.UserRegistry) UserFileService {
+	return userFileService{ur}
+}
